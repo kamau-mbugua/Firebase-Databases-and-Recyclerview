e11325d4225b6b9c6d48298975fdd0147c7e9cdc
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-01-05 00:25:30 +0300

    Implementing the RecyclerAdapter

--- app/src/main/java/com/example/searchhotel/HotelModel.java
+++ app/src/main/java/com/example/searchhotel/HotelModel.java
@@ -1,4 +1,91 @@
 package com.example.searchhotel;
 
 public class HotelModel {
+    public  String hotelLocation;
+    public String hotelName;
+    public String imageUri;
+    public  String hotelRating;
+    public String hotelListTag;
+    public String hotelPricePerHour;
+    private String key;
+    private String ID;
+
+
+    public HotelModel() {
+
+    }
+
+    public HotelModel(String hotelLocation, String hotelName, String imageUri, String hotelRating, String hotelListTag, String hotelPricePerHour) {
+        this.hotelLocation = hotelLocation;
+        this.hotelName = hotelName;
+        this.imageUri = imageUri;
+        this.hotelRating = hotelRating;
+        this.hotelListTag = hotelListTag;
+        this.hotelPricePerHour = hotelPricePerHour;
+
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getHotelLocation() {
+        return hotelLocation;
+    }
+
+    public void setHotelLocation(String hotelLocation) {
+        this.hotelLocation = hotelLocation;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public String getImageUri() {
+        return imageUri;
+    }
+
+    public void setImageUri(String imageUri) {
+        this.imageUri = imageUri;
+    }
+
+    public String getHotelRating() {
+        return hotelRating;
+    }
+
+    public void setHotelRating(String hotelRating) {
+        this.hotelRating = hotelRating;
+    }
+
+    public String getHotelListTag() {
+        return hotelListTag;
+    }
+
+    public void setHotelListTag(String hotelListTag) {
+        this.hotelListTag = hotelListTag;
+    }
+
+    public String getHotelPricePerHour() {
+        return hotelPricePerHour;
+    }
+
+    public void setHotelPricePerHour(String hotelPricePerHour) {
+        this.hotelPricePerHour = hotelPricePerHour;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
 }
--- app/src/main/java/com/example/searchhotel/RecyclerAdapter.java
+++ app/src/main/java/com/example/searchhotel/RecyclerAdapter.java
@@ -1,4 +1,74 @@
 package com.example.searchhotel;
 
-public class RecyclerAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.squareup.picasso.Picasso;
+
+public class RecyclerAdapter extends FirebaseRecyclerAdapter<HotelModel,RecyclerAdapter.myviewholder>
+{
+
+    public RecyclerAdapter(@NonNull FirebaseRecyclerOptions<HotelModel> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull myviewholder holder, int position, @NonNull final HotelModel model) {
+       /* holder.nametext.setText(model.getName());
+        holder.coursetext.setText(model.getCourse());
+        holder.emailtext.setText(model.getEmail());
+        Glide.with(holder.img1.getContext()).load(model.getPurl()).into(holder.img1);*/
+        holder.ratings.setText(model.getHotelRating());
+        holder.hotelLocation.setText(model.getHotelLocation());
+        holder.hotelName.setText(model.getHotelName());
+        holder.tagsList.setText(model.getHotelListTag());
+        Picasso.with(holder.hotelImage.getContext())
+                .load(model.getImageUri())
+                .placeholder(R.drawable.placeholder)
+                .fit()
+                .centerCrop()
+                .into(holder.hotelImage);
+
+        holder.hotelImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AppCompatActivity activity=(AppCompatActivity)view.getContext();
+                activity.getSupportFragmentManager().beginTransaction().replace(R.id.wrap,new DescriptionFragment(model.getHotelListTag(),model.getHotelLocation(),model.getHotelName(),model.getHotelRating(), model.getImageUri())).addToBackStack(null).commit();
+            }
+        });
+    }
+
+    @NonNull
+    @Override
+    public myviewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.row_layout,parent,false);
+        return new myviewholder(view);
+    }
+
+    public class myviewholder extends RecyclerView.ViewHolder
+    {
+        public TextView ratings,hotelLocation,hotelName,tagsList;
+        public ImageView hotelImage;
+
+        public myviewholder(@NonNull View itemView) {
+            super(itemView);
+
+            ratings =itemView.findViewById ( R.id.ratings );
+            hotelLocation = itemView.findViewById(R.id.hotelLocation);
+            hotelLocation = itemView.findViewById(R.id.hotelLocation);
+            hotelName = itemView.findViewById(R.id.hotelName);
+            tagsList = itemView.findViewById(R.id.tagsList);
+            hotelImage = itemView.findViewById(R.id.hotelImage);
+        }
+    }
+
 }
